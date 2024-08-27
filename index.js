b73c04fb1908497f6015a89cc9934d7af9332fa7
Utsav Seal <51076804+UtsavSeal@users.noreply.github.com>
2024-08-27 21:36:37 +0530

    Update index.js
    I just made the audio a little bit better i think i dont for sure lmao

--- index.js
+++ index.js
@@ -17,31 +17,31 @@ function makeSound(key){
 
         case "w":
             let crash = new Audio("./sounds/crash.mp3");
-            crash.play();
+            crash.cloneNode().play();
             break;
         case "a":
             let kick = new Audio("./sounds/kick-bass.mp3");
-            kick.play();
+            kick.cloneNode().play();
             break;
         case "s":
             let snare = new Audio("./sounds/snare.mp3");
-            snare.play(); 
+            snare.cloneNode().play();; 
             break;
         case "d":
             let tom1 = new Audio("./sounds/tom-1.mp3");
-            tom1.play();
+            tom1.cloneNode().play();
             break;
         case "j":
             let tom2 = new Audio("./sounds/tom-2.mp3");
-            tom2.play();
+            tom2.cloneNode().play();
             break;
         case "k":
             let tom3 = new Audio("./sounds/tom-3.mp3");
-            tom3.play();
+            tom3.cloneNode().play();
             break;
         case "l":
             let tom4 = new Audio("./sounds/tom-4.mp3");
-            tom4.play();
+            tom4.cloneNode().play();
             break;      
     }
 }
